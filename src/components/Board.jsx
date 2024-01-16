c24cfb22c15673519f88a5f27b3c922ec00b240e
Orgil <mnafairy@gmail.com>
2024-01-16 20:03:39 +0800

    jan 16 8pm

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/components/Board.module.css";
 import { useState } from "react";
 import Card from "@/components/Card";
+import AddScreen from "@/components/AddScreen";
 const players = [
   {
     id: 1,
@@ -25,7 +26,9 @@ const players = [
 ];
 export function Board() {
   const [scores, setScores] = useState(players);
-
+  let [clicked, setClick] = useState(false);
+  const [playerName, setPlayerName] = useState("");
+  const [playerScore, setPlayerScore] = useState(0);
   const increase = (id) => {
     let updateScores = scores.map((score) => {
       if (score.id == id) {
@@ -56,6 +59,40 @@ export function Board() {
     console.log("updateScores:", updateScores);
     setScores(updateScores);
   };
+  const showAddPlayer = () => {
+    console.log(clicked);
+    clicked = !clicked;
+    setClick(clicked);
+  };
+  const handleChange = (event, type) => {
+    type == "name"
+      ? setPlayerName(event.target.value)
+      : type == "score"
+      ? setPlayerScore(event.target.value)
+      : null;
+  };
+  const addPlayer = () => {
+    let nameValid = false;
+    let scoreValid = false;
+    let newPlayer = {
+      id: scores.length,
+      pName: playerName,
+      pScore: playerScore,
+    };
+    console.log(typeof playerScore);
+    isNaN(newPlayer.pScore)
+      ? alert("Please enter a valid score")
+      : (scoreValid = true);
+    playerName.includes("John")
+      ? (nameValid = true)
+      : alert("Please enter a name that includes John");
+    if (nameValid && scoreValid) {
+      scores.push(newPlayer);
+      scores.sort((b, a) => a.pScore - b.pScore);
+      setScores([...scores]);
+    }
+    console.log(scores);
+  };
   return (
     <div className={styles.cardContainer}>
       {scores.map((player, index) => {
@@ -70,6 +107,12 @@ export function Board() {
           />
         );
       })}
+      <AddScreen
+        bool={clicked}
+        addFunc={addPlayer}
+        setPlayerName={handleChange}
+      />
+      <button onClick={() => showAddPlayer()}>Add player</button>
       <button onClick={() => reset()}>Reset</button>
     </div>
   );
--- src/pages/tailwindcss.js
+++ src/pages/tailwindcss.js
@@ -1,7 +1,7 @@
-export const Tailwind = () => {
-  return (
-    <h1 className="text-3xl font-bold underline text-green-200 p-2 ">
-      Hello world!
-    </h1>
-  );
-};
+// export const Tailwind = () => {
+//   return (
+//     <h1 className="text-3xl font-bold underline text-green-200 p-2 ">
+//       Hello world!
+//     </h1>
+//   );
+// };
